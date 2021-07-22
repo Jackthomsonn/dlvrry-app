a37f0533c9add4d72ae7a687a5b6806e20cbcc6c
jackthomsonn <hello@jackthomson.co.uk>
2021-07-22 23:39:17 +0100

    update 2

--- src/pages/add-card/AddCard.tsx
+++ src/pages/add-card/AddCard.tsx
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { WebView } from 'react-native-webview';
 import { useNavigation } from "@react-navigation/native";
 import Constants from 'expo-constants';
+import firebase from 'firebase';
+import { Loader } from '../../components/loader';
 
 export function AddCardScreen(props: any) {
   const navigation = useNavigation();
-  const [ inProgress, setInProgress ] = useState(true);
+  const [inProgress, setInProgress] = useState(true);
+  const [token, setToken] = useState(undefined);
 
   const url = Constants.manifest.extra.useEmulator
     ? 'https://dlvrry-payment-hgwx8lxfr-jackthomson.vercel.app'
     : 'https://payment.dlvrry.io'
 
+  const getToken = async () => {
+    const token = await firebase.auth().currentUser.getIdToken();
+
+    setToken(token);
+  }
+
+  useEffect(() => {
+    getToken();
+  }, [])
+
   return (
-    <WebView
-      originWhitelist={[ '*' ]}
-      source={{ uri: `${url}?customer_id=${ props.route.params.customer_id }` }}
-      onNavigationStateChange={async (e) => {
-        if (e.url.includes('success=true')) {
-          navigation.goBack();
-        } else {
-          if (e.url.includes('goback=true')) {
-            if (inProgress) {
-              setInProgress(false);
-              navigation.goBack();
+
+    token
+      ? <WebView
+        originWhitelist={['*']}
+        source={{ uri: `${url}?customer_id=${props.route.params.customer_id}?${token}` }
+        }
+        onNavigationStateChange={async (e) => {
+          if (e.url.includes('success=true')) {
+            navigation.goBack();
+          } else {
+            if (e.url.includes('goback=true')) {
+              if (inProgress) {
+                setInProgress(false);
+                navigation.goBack();
+              }
             }
           }
         }
-      }}
-    />
+        }
+      />
+      : <Loader />
   );
 }
